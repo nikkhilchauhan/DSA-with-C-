90276ed6379213895787273d3bb1430a17ef8090
nikkhilchauhan <nikhilchauhan0018@gmail.com>
2022-09-25 13:26:17 +0530

    25-Sept-2022

---
+++ pattern_3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int i = 1;
+    cin >> n;
+    while (i<=n)
+    {
+        int j = 1;
+        while(j<=n){
+            cout << j;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
+
+// 12345
+// 12345
+// 12345
+// 12345
+// 12345
